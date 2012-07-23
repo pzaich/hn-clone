69eced3508dd39b40f6fef7c428477fc8c325074
Brett Camarda <bcamarda@gmail.com>
2012-07-23 16:12:02 -0700

    added form for submission

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,9 +5,17 @@ class LinksController < ApplicationController
 	end
 
 	def new
+	  @link = Link.new
 	end
 
 	def create
+	  @link = Link.new(params[:link])
+    if @link.save
+      flash[:messages] = "Thank you for your submission!"
+      redirect_to root_path
+    else
+      render :new
+    end
 	end
 
 	def show
