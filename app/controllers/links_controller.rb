9186e78cf56fdc3eb6590562cd35361becfff661
Paul Zaich <pzaich@gmail.com>
2012-07-23 16:10:38 -0700

    pagination

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,7 +1,7 @@
 class LinksController < ApplicationController
 
 	def index
-	  @links = Link.all
+	  @links = Kaminari::paginate_array(Link.find(:all)).page(params[:page]).per(10)
 	end
 
 	def new
