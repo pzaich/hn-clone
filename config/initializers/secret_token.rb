bb61c17b077d7f7d60e933f7a7fed75615089e83
Paul Zaich <pzaich@gmail.com>
2012-07-23 10:48:24 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HnClone::Application.config.secret_token = '216da5cf8caa1460b8e15e50cdc5a75096e2156c5442b6552d16d651875aadb835b8698f827e67f873c8f646b748ff74adae38b7d86984694e239a2c01b78bff'
