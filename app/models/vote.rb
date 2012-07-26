d27afb3202e2d8a093a3638a9edbb355f69920eb
Brett Camarda <bcamarda@gmail.com>
2012-07-25 21:42:59 -0700

    votes record a value

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,15 +1,15 @@
 class Vote < ActiveRecord::Base
-  attr_accessible :user_id, :voteable_id
+  attr_accessible :user_id, :voteable_id, :value
 
   belongs_to :user
   belongs_to :voteable, :polymorphic => true
   
   validates_uniqueness_of :voteable_id, :scope => :user_id
-  validates_inclusion_of :value in: [1, -1]
+  validates_inclusion_of :value, :in => [1, -1]
   validate :not_the_author
   
   def not_the_author
-    errors.add :user_id, "is the author" if voteable.user_id == user_id
+    errors.add :user_id, "is the author" if voteable.user_id == user
   end
   
 
