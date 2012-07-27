e700f93e2f08b2a2fe132d5ea03ac02dc0c1e5e9
Paul Zaich <pzaich@gmail.com>
2012-07-27 16:29:09 -0700

    cleaned up header into partial, added bootstrap gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 	def down_vote_link(voteable)
 		if user_signed_in?
-			link_to "Down", link_votes_path(voteable, :value => -1), :method => :post if current_user.karma_points > 100 
+			link_to "Down", link_votes_path(voteable, :value => -1), :method => :post, :class => "icon-arrow-down" if current_user.karma_points > 100 
 		end
 	end
 
 	def up_vote_link(voteable)
-		link_to "Up",  link_votes_path(voteable, :value => 1), :method => :post if user_signed_in?
+		link_to "",  link_votes_path(voteable, :value => 1), :method => :post, :class => "icon-arrow-up" if user_signed_in?
 	end
 end
