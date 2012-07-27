1f29704a89a80b33165fae3e2b97e181c4db6cd0
Paul Zaich <pzaich@gmail.com>
2012-07-26 17:30:34 -0700

    remove the down vote from views

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,2 +1,7 @@
 module LinksHelper
+		def down_vote_link(voteable)
+		if user_signed_in?
+			link_to "Down", link_votes_path(voteable, :value => -1), :method => :post if current_user.karma_points > 100 
+		end
+	end
 end
