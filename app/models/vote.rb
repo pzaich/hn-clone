4020199cabf8a56be3b97c6b71f548797f32445d
Paul Zaich <pzaich@gmail.com>
2012-07-27 14:31:48 -0700

    clean up the routes on comments

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -18,7 +18,9 @@ class Vote < ActiveRecord::Base
   end
 
   def remove_karma
-  	self.voteable.user.decrement!(:karma_points) unless self.voteable.user.karma_points == 0
+    if self.user.karma_points > 99 
+  	 self.voteable.user.decrement!(:karma_points) unless self.voteable.user.karma_points == 0
+    end
   end
 
   def give_karma
