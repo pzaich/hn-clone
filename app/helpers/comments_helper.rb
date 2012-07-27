cca798d0ad5db55c96cb79c2af690c5a69f99b49
Paul Zaich <pzaich@gmail.com>
2012-07-27 15:26:34 -0700

    extract links on index page into partials

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -4,5 +4,10 @@ module CommentsHelper
 		if user_signed_in?
 			link_to "Down", link_votes_path(voteable, :value => -1), :method => :post if current_user.karma_points > 100 
 		end
+
+		
+		def up_vote_link(voteable)
+			link_to "Up",  link_votes_path(voteable, :value => 1), :method => :post if user_signed_in?
+		end
 	end
 end
--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,7 +1,11 @@
 module LinksHelper
-		def down_vote_link(voteable)
+	def down_vote_link(voteable)
 		if user_signed_in?
 			link_to "Down", link_votes_path(voteable, :value => -1), :method => :post if current_user.karma_points > 100 
 		end
+	end
+
+	def up_vote_link(voteable)
+		link_to "Up",  link_votes_path(voteable, :value => 1), :method => :post if user_signed_in?
 	end
 end
