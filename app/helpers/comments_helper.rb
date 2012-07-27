b0f8915bd704ff4f49f53ee00372710908d1eed5
Paul Zaich <pzaich@gmail.com>
2012-07-27 15:32:08 -0700

    moved helpers to applicationhelper

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,13 +1,3 @@
 module CommentsHelper
-  
-  	def down_vote_link(voteable)
-		if user_signed_in?
-			link_to "Down", link_votes_path(voteable, :value => -1), :method => :post if current_user.karma_points > 100 
-		end
-
-		
-		def up_vote_link(voteable)
-			link_to "Up",  link_votes_path(voteable, :value => 1), :method => :post if user_signed_in?
-		end
-	end
+ 
 end
