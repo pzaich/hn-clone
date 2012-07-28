4eceaa3af1b43b18b39f2c002157203edfd4bb9f
Paul Zaich <pzaich@gmail.com>
2012-07-27 17:44:45 -0700

    readme

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
 	def down_vote_link(voteable)
 		if user_signed_in?
-			link_to "Down", link_votes_path(voteable, :value => -1), :method => :post, :class => "icon-arrow-down" if current_user.karma_points > 100 
+			link_to "Down", link_votes_path(voteable, :value => -1), :method => :post, :class => "icon-arrow-down", :remote => true if current_user.karma_points > 100 
 		end
 	end
 
 	def up_vote_link(voteable)
 	  if user_signed_in?
 		  if voteable.votes.map{|vote| vote.user_id}.include?(current_user.id)
-		    link_to "",  link_votes_path(voteable, :value => 1), :method => :post, :class => "icon-arrow-up"
+		    link_to "",  link_votes_path(voteable, :value => 1), :method => :post, :class => "icon-arrow-up", :remote => true
 	    end
     end
 	end
---
+++ readme.mdx
@@ -0,0 +1,5 @@
+This is a clone of the Hacker News app (new.ycombinator.com) & Reddit (reddit.com) using Rails 3. 
+
+It demonstrates polymorphic relationships between Articles/Commments & Comments/Comments AND Articles/Votes & Comments/Votes. 
+
+Users are also assigned a value based off of votes on their article submissions.
